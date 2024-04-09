b972ab90a9652c333b2db37def8dd2d57afce581
Высочанский Максим <126272688+Maxxx-VS@users.noreply.github.com>
2024-04-09 21:55:58 +0600

    #2 Добавлены рубрики

--- samplesite/bboard/admin.py
+++ samplesite/bboard/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
+from .models import Bd, Rubric
 
-from .models import Bd
+class BdAdmin(admin.ModelAdmin):
+    list_display = ('rubric', 'title', 'content', 'price', 'published')
+    list_display_links = ('title', 'content')
+    search_fields = ('title', 'content')
 
-admin.site.register(Bd)
+
+admin.site.register(Bd, BdAdmin)
+admin.site.register(Rubric)
--- samplesite/bboard/views.py
+++ samplesite/bboard/views.py
@@ -1,7 +1,15 @@
 # from django.http import HttpResponse
 # from django.template import loader
 from django.shortcuts import render
-from .models import Bd
+from .models import Bd, Rubric
+
+
+def rubric_bbs(request, rubric_id):
+    bbs = Bd.objects.filter(rubric=rubric_id)
+    rubrics = Rubric.objects.all()
+    current_rubric = Rubric.objects.get(pk=rubric_id)
+    context = {'bbs': bbs, 'rubrics': rubrics, 'current_rubric': current_rubric}
+    return render(request, 'bboard/rubric_bbs.html', context)
 
 def index(request):
     bbs = Bd.objects.all()
